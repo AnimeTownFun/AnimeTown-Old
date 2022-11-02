1337c441d1ee3902a955248f53163cbbe86f8161
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2022-11-02 20:08:47 +0600

    Updating chacker

--- functions/check_port.js
+++ functions/check_port.js
@@ -1,18 +1,18 @@
-module.exports = async function (port){
-  console.log("CHECKING PORT...");
-  try{
+const chalk = require("chalk");
+const Coral = chalk.hex("#FF7F50");
+
+module.exports = async function (port) {
+  console.log(chalk.yellow("CHECKING PORT..."));
+  try {
     let portx = isNaN(port);
-    if(portx === false)
-    {
-      console.log("CHECK SUCCESSFULL. PORT IS VALID.")
+    if (portx === false) {
+      console.log(Coral("CHECK SUCCESSFULL. PORT IS VALID."));
       return Number(port);
     } else {
-      console.log("CHECK SUCCESSFULL. PORT IS INVALID");
+      console.log(chalk.red("CHECK SUCCESSFULL. PORT IS INVALID"));
       return Number(port);
     }
-    
-    
-  } catch(e) {
-    console.log(e)
+  } catch (e) {
+    console.log(e);
   }
-}
+};
--- functions/update_list.js
+++ functions/update_list.js
@@ -1,33 +1,32 @@
 const axios = require("axios");
 const db = require("quick.db");
+const chalk = require("chalk");
+const AliceBlue = chalk.hex("#F0F8FF");
 module.exports = async function (api_url, secs) {
-  if(!secs.includes("disable")) {
-console.log("UPDATING POPULAR AND RECENTLY ADDED DATABASE");
+  if (!secs.includes("disable")) {
+    console.log(AliceBlue("UPDATING POPULAR AND RECENTLY ADDED DATABASE"));
   }
-  try { 
+  try {
+    let resx = await axios.get(api_url + "api/recentlyadded/1");
 
-     let resx = await axios.get(api_url +"api/recentlyadded/1");
+    let res = await axios.get(api_url + "api/popular/1");
 
-  
-  let res = await axios.get(api_url +"api/popular/1");
+    if (!secs.includes("disable")) {
+      db.set(`popular`, res.data.results);
 
- 
-if(!secs.includes("disable")) {
-   db.set(`popular`, res.data.results);
-
-   db.set(`recentlyadded`, resx.data.results);
-}
-      if(!secs.includes("disable")) {
-console.log("UPDATED POPULAR AND RECENTLY ADDED DATABASE");
-  }
-  return {
-    popular: res.data.results,
-    recent: resx.data.results,
-  }
-     } catch(e) {
-    console.log(e);
+      db.set(`recentlyadded`, resx.data.results);
+    }
+    if (!secs.includes("disable")) {
+      console.log(AliceBlue("UPDATED POPULAR AND RECENTLY ADDED DATABASE"));
+    }
     return {
-    error: "404",
+      popular: res.data.results,
+      recent: resx.data.results,
+    };
+  } catch (e) {
+    console.log(chalk.red(e));
+    return {
+      error: "404",
+    };
   }
-  }
-}
+};
