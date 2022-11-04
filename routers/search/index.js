c74ec2da8368a48d5d7d31bc2a84ce23b58769e7
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2022-11-04 21:38:36 +0600

    Search Response Edited

--- routers/search/index.js
+++ routers/search/index.js
@@ -19,7 +19,7 @@ app.get("/", async (req, res) => {
     return res.render("search.ejs", { popular: dat });
   } catch (e) {
     console.log(chalk.red(e));
-    return res.send("<h1> 404 </h1>");
+    return res.send("<h1> 404 </h1><br><p>Something Went Wrong...</p>");
   }
 });
 
