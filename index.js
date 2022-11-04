cd8320d66bb9b0cc054314c4a6677449502c5cc1
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2022-11-04 21:26:53 +0600

    Error handling

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-require("./app.js")();
+const { exec } = require("child_process");
+
+try {
+  require("./app.js")();
+} catch (err) {
+  console.log(err);
+  process.exit &&
+    exec("npm start", (error, stderr, stdout) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+}
